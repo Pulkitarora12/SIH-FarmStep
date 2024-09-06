27ed323004c18b890aaeea6974d6dee3aa5a0424
Ashish-Kumar-Vaish <ashishkv1715@gmail.com>
2024-09-07 00:46:40 +0530

    first code

---
+++ dasahboard/dashboard.js
@@ -0,0 +1,10 @@
+console.log("connected to js file")
+
+function openPage(url) {
+    window.location.href = url;
+}
+
+function showProfile() {
+    alert("Profile clicked!");
+    // Implement profile display logic here
+}
